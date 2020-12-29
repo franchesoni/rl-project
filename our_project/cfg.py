eb456b0014c0d4c3acbadc4d8e86cbd0e9607fb2
Clément Bonnet <clement.bonnet16@gmail.com>
2020-12-29 12:48:18 +0100

    config file

--- our_project/cfg.py
+++ our_project/cfg.py
@@ -1,3 +1,4 @@
+import json
 import random
 import os
 
@@ -7,20 +8,24 @@ from torch.utils.tensorboard import SummaryWriter
 
 #####################
 
-N_INTERACTIONS = 1
-# Addition config
-CURRICULUM = [[0, 1]]
-MAX_DIGITS = 2
-TRAIN_SIZE=10000
-VAL_SIZE=1000
-BATCH_SIZE=320
-EPOCHS=100
-NUM_CHARS = 12
-LR=0.001
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+N_INTERACTIONS = config["N_INTERACTIONS"]
+CURRICULUM = config["CURRICULUM"]
+MAX_DIGITS = config["MAX_DIGITS"]
+TRAIN_SIZE = config["TRAIN_SIZE"]
+VAL_SIZE = config["VAL_SIZE"]
+BATCH_SIZE = config["BATCH_SIZE"]
+EPOCHS = config["EPOCHS"]
+NUM_CHARS = config["NUM_CHARS"]
+LR = config["LR"]
+SUMMARY_WRITER_PATH = config["SUMMARY_WRITER_PATH"]
+WRITER = SummaryWriter(SUMMARY_WRITER_PATH)
+SEED = config["SEED"]
 
 #####################
 
-SEED = 42
 def seed_everything(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -30,9 +35,3 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 seed_everything(SEED)
-
-##################
-
-WRITER = SummaryWriter('./runs/directly_two')
-
-###############
