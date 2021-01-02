e0b6db39eafacf7a7deb31c46c4d630fde09d227
clement-bonnet <clement.bonnet16@gmail.com>
2021-01-03 00:07:26 +0100

    prepared all direct and baseline runs

--- our_project/run_all.sh
+++ our_project/run_all.sh
@@ -1,9 +1,21 @@
-python3 main.py --config-file config_one_direct.json
-python3 main.py --config-file config_two_direct.json
-python3 main.py --config-file config_three_direct.json
-python3 main.py --config-file config_four_direct.json
-python3 main.py --config-file config_five_direct.json
-python3 main.py --config-file config_six_direct.json
-python3 main.py --config-file config_seven_direct.json
-python3 main.py --config-file config_eight_direct.json
-python3 main.py --config-file config_nine_direct.json
+### [direct] ###
+python3 main.py --config-file direct/config_1_direct.json
+python3 main.py --config-file direct/config_2_direct.json
+python3 main.py --config-file direct/config_3_direct.json
+python3 main.py --config-file direct/config_4_direct.json
+python3 main.py --config-file direct/config_5_direct.json
+python3 main.py --config-file direct/config_6_direct.json
+python3 main.py --config-file direct/config_7_direct.json
+python3 main.py --config-file direct/config_8_direct.json
+python3 main.py --config-file direct/config_9_direct.json
+
+### [baseline]
+python3 main.py --config-file baseline/config_1_baseline.json
+python3 main.py --config-file baseline/config_2_baseline.json
+python3 main.py --config-file baseline/config_3_baseline.json
+python3 main.py --config-file baseline/config_4_baseline.json
+python3 main.py --config-file baseline/config_5_baseline.json
+python3 main.py --config-file baseline/config_6_baseline.json
+python3 main.py --config-file baseline/config_7_baseline.json
+python3 main.py --config-file baseline/config_8_baseline.json
+python3 main.py --config-file baseline/config_9_baseline.json
