81c04617656ad949e8a59854f6bfabcc54f322bc
clement-bonnet <clement.bonnet16@gmail.com>
2021-01-03 09:38:31 +0100

    added combined config curriculum

--- our_project/run_all.sh
+++ our_project/run_all.sh
@@ -1,21 +1,32 @@
-### [direct] ###
-python3 main.py --config-file direct/config_1_direct.json
-python3 main.py --config-file direct/config_2_direct.json
-python3 main.py --config-file direct/config_3_direct.json
-python3 main.py --config-file direct/config_4_direct.json
-python3 main.py --config-file direct/config_5_direct.json
-python3 main.py --config-file direct/config_6_direct.json
-python3 main.py --config-file direct/config_7_direct.json
-python3 main.py --config-file direct/config_8_direct.json
-python3 main.py --config-file direct/config_9_direct.json
+# ### [direct] ###
+# python3 main.py --config-file direct/config_1_direct.json
+# python3 main.py --config-file direct/config_2_direct.json
+# python3 main.py --config-file direct/config_3_direct.json
+# python3 main.py --config-file direct/config_4_direct.json
+# python3 main.py --config-file direct/config_5_direct.json
+# python3 main.py --config-file direct/config_6_direct.json
+# python3 main.py --config-file direct/config_7_direct.json
+# python3 main.py --config-file direct/config_8_direct.json
+# python3 main.py --config-file direct/config_9_direct.json
 
-### [baseline]
-python3 main.py --config-file baseline/config_1_baseline.json
-python3 main.py --config-file baseline/config_2_baseline.json
-python3 main.py --config-file baseline/config_3_baseline.json
-python3 main.py --config-file baseline/config_4_baseline.json
-python3 main.py --config-file baseline/config_5_baseline.json
-python3 main.py --config-file baseline/config_6_baseline.json
-python3 main.py --config-file baseline/config_7_baseline.json
-python3 main.py --config-file baseline/config_8_baseline.json
-python3 main.py --config-file baseline/config_9_baseline.json
+# ### [baseline]
+# python3 main.py --config-file baseline/config_1_baseline.json
+# python3 main.py --config-file baseline/config_2_baseline.json
+# python3 main.py --config-file baseline/config_3_baseline.json
+# python3 main.py --config-file baseline/config_4_baseline.json
+# python3 main.py --config-file baseline/config_5_baseline.json
+# python3 main.py --config-file baseline/config_6_baseline.json
+# python3 main.py --config-file baseline/config_7_baseline.json
+# python3 main.py --config-file baseline/config_8_baseline.json
+# python3 main.py --config-file baseline/config_9_baseline.json
+
+### [combined]
+python3 main.py --config-file combined/config_1_combined.json
+python3 main.py --config-file combined/config_2_combined.json
+python3 main.py --config-file combined/config_3_combined.json
+python3 main.py --config-file combined/config_4_combined.json
+python3 main.py --config-file combined/config_5_combined.json
+python3 main.py --config-file combined/config_6_combined.json
+python3 main.py --config-file combined/config_7_combined.json
+python3 main.py --config-file combined/config_8_combined.json
+python3 main.py --config-file combined/config_9_combined.json
