d0e96ffc66288852ebbdaea645233c7d1d00f193
Clément Bonnet <clement.bonnet16@gmail.com>
2021-02-05 23:34:53 +0100

    final clean

---
+++ our_project/run_example.sh
@@ -0,0 +1,32 @@
+### [direct] ###
+# python3 main.py --config-file old_configs/direct/config_1_direct.json
+# python3 main.py --config-file old_configs/direct/config_2_direct.json
+# python3 main.py --config-file old_configs/direct/config_3_direct.json
+# python3 main.py --config-file old_configs/direct/config_4_direct.json
+# python3 main.py --config-file old_configs/direct/config_5_direct.json
+# python3 main.py --config-file old_configs/direct/config_6_direct.json
+# python3 main.py --config-file old_configs/direct/config_7_direct.json
+# python3 main.py --config-file old_configs/direct/config_8_direct.json
+# python3 main.py --config-file old_configs/direct/config_9_direct.json
+
+### [baseline]
+# python3 main.py --config-file old_configs/baseline/config_1_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_2_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_3_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_4_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_5_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_6_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_7_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_8_baseline.json
+# python3 main.py --config-file old_configs/baseline/config_9_baseline.json
+
+### [combined]
+# python3 main.py --config-file old_configs/combined/config_1_combined.json
+# python3 main.py --config-file old_configs/combined/config_2_combined.json
+# python3 main.py --config-file old_configs/combined/config_3_combined.json
+# python3 main.py --config-file old_configs/combined/config_4_combined.json
+# python3 main.py --config-file old_configs/combined/config_5_combined.json
+# python3 main.py --config-file old_configs/combined/config_6_combined.json
+# python3 main.py --config-file old_configs/combined/config_7_combined.json
+# python3 main.py --config-file old_configs/combined/config_8_combined.json
+# python3 main.py --config-file old_configs/combined/config_9_combined.json
